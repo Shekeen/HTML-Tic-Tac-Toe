a50f3dd2d69c00bc01a537cd316dedbcc838ec9c
Shekeen <guryanov91@gmail.com>
2013-10-18 00:41:35 +0400

    Array of cells

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,9 @@
 $(document).ready(function(){
     var crosses = 0,
         circles = 0;
-    var $cell11 = $('#cell11'),
-        $cell12 = $('#cell12'),
-        $cell13 = $('#cell13'),
-        $cell21 = $('#cell21'),
-        $cell22 = $('#cell22'),
-        $cell23 = $('#cell23'),
-        $cell31 = $('#cell31'),
-        $cell32 = $('#cell32'),
-        $cell33 = $('#cell33');
+    var $cells = [[$('#cell11'), $('#cell12'), $('#cell13')],
+                  [$('#cell21'), $('#cell22'), $('#cell23')],
+                  [$('#cell31'), $('#cell32'), $('#cell33')]];
     var $turn = $('span#turn');
 
     $('span.cell').click(function() {
